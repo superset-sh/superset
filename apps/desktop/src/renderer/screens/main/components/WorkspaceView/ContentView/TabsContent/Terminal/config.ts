4b1670ba357c96c2c58e725ed478a77a28c4317b
Kiet Ho <hoakiet98@gmail.com>
2025-11-26 12:17:59 -0600

    remove custom xterm theme

--- apps/desktop/src/renderer/screens/main/components/WorkspaceView/ContentView/TabsContent/Terminal/config.ts
+++ apps/desktop/src/renderer/screens/main/components/WorkspaceView/ContentView/TabsContent/Terminal/config.ts
@@ -1,27 +1,7 @@
 import type { ITerminalOptions } from "@xterm/xterm";
 
-export const TERMINAL_THEME: ITerminalOptions["theme"] = {
-	background: "#000000",
-	foreground: "#d4d4d4",
-	cursor: "#d4d4d4",
-	black: "#000000",
-	red: "#cd3131",
-	green: "#0dbc79",
-	yellow: "#e5e510",
-	blue: "#2472c8",
-	magenta: "#bc3fbc",
-	cyan: "#11a8cd",
-	white: "#e5e5e5",
-	brightBlack: "#666666",
-	brightRed: "#f14c4c",
-	brightGreen: "#23d18b",
-	brightYellow: "#f5f543",
-	brightBlue: "#3b8eea",
-	brightMagenta: "#d670d6",
-	brightCyan: "#29b8db",
-	brightWhite: "#e5e5e5",
-};
-
+// Use user's theme
+export const TERMINAL_THEME: ITerminalOptions["theme"] = undefined;
 export const TERMINAL_OPTIONS: ITerminalOptions = {
 	cursorBlink: true,
 	fontSize: 14,
