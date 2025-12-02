175daf3a60df243e1d11e9846cc20ab8dfca928e
Kiet Ho <hoakiet98@gmail.com>
2025-12-01 17:03:32 -0800

    format

--- apps/website/src/constants.ts
+++ apps/website/src/constants.ts
@@ -1,3 +1,4 @@
-export const DOWNLOAD_URL_MAC_ARM64 = "https://github.com/superset-sh/superset/releases/latest/download/Superset-arm64-mac.zip";
+export const DOWNLOAD_URL_MAC_ARM64 =
+	"https://github.com/superset-sh/superset/releases/latest/download/Superset-arm64-mac.zip";
 
 export const GITHUB_REPO_URL = "https://github.com/superset-sh/superset";
