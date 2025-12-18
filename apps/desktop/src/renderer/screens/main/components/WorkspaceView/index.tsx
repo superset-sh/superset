8191880a4a6f2bfe28806a9ca70030ecbb143b25
Kiet Ho <hoakiet98@gmail.com>
2025-12-17 17:44:43 -0800

    remove redundants

--- apps/desktop/src/renderer/screens/main/components/WorkspaceView/index.tsx
+++ apps/desktop/src/renderer/screens/main/components/WorkspaceView/index.tsx
@@ -107,11 +107,11 @@ export function WorkspaceView() {
 	}, [isSidebarOpen]);
 
 	return (
-		<div className="flex-1 h-full flex flex-col min-h-0">
+		<div className="flex-1 h-full flex flex-col">
 			<WorkspaceHeader worktreePath={activeWorkspace?.worktreePath} />
 			<ResizablePanelGroup
 				direction="horizontal"
-				className="flex-1 min-h-0 bg-tertiary"
+				className="flex-1 bg-tertiary"
 			>
 				<ResizablePanel
 					ref={sidebarPanelRef}
