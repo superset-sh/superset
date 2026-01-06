bb36fde65f7f3bfdb9595839bf03b394cd8a7c9f
Satya Patel <satyapatel111@gmail.com>
2026-01-06 10:11:30 -0800

    WIP - tried to restore code

--- apps/desktop/src/renderer/contexts/AppProviders/AppProviders.tsx
+++ apps/desktop/src/renderer/contexts/AppProviders/AppProviders.tsx
@@ -1,7 +1,6 @@
 import type React from "react";
 import { PostHogUserIdentifier } from "renderer/components/PostHogUserIdentifier";
 import { MonacoProvider } from "../MonacoProvider";
-import { OrganizationsProvider } from "../OrganizationsProvider";
 import { PostHogProvider } from "../PostHogProvider";
 import { TRPCProvider } from "../TRPCProvider";
 
@@ -13,10 +12,8 @@ export function AppProviders({ children }: AppProvidersProps) {
 	return (
 		<PostHogProvider>
 			<TRPCProvider>
-				<OrganizationsProvider>
-					<PostHogUserIdentifier />
-					<MonacoProvider>{children}</MonacoProvider>
-				</OrganizationsProvider>
+				<PostHogUserIdentifier />
+				<MonacoProvider>{children}</MonacoProvider>
 			</TRPCProvider>
 		</PostHogProvider>
 	);
--- apps/desktop/src/renderer/contexts/MonacoProvider/index.ts
+++ apps/desktop/src/renderer/contexts/MonacoProvider/index.ts
@@ -3,4 +3,6 @@ export {
 	monaco,
 	SUPERSET_THEME,
 	useMonacoReady,
+	MONACO_EDITOR_OPTIONS,
+	registerSaveAction,
 } from "./MonacoProvider";
