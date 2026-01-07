5d5f67d092a8c6c71acd8e2538d78a05ab23ff79
Satya Patel <satyapatel111@gmail.com>
2026-01-06 21:30:07 -0800

    WIP

--- apps/desktop/src/renderer/contexts/MonacoProvider/index.ts
+++ apps/desktop/src/renderer/contexts/MonacoProvider/index.ts
@@ -1,8 +1,8 @@
 export {
+	MONACO_EDITOR_OPTIONS,
 	MonacoProvider,
 	monaco,
+	registerSaveAction,
 	SUPERSET_THEME,
 	useMonacoReady,
-	MONACO_EDITOR_OPTIONS,
-	registerSaveAction,
 } from "./MonacoProvider";
