837eebe9e596f9e36a926011b9771f42d8112ec1
Satya Patel <satyapatel111@gmail.com>
2026-01-31 13:06:34 -0800

    move slack-billing into barrel folder per project structure guidelines

---
+++ packages/auth/src/utils/slack-billing/index.ts
@@ -0,0 +1,8 @@
+export {
+	formatPaymentFailed,
+	formatPaymentSucceeded,
+	formatPlanChanged,
+	formatSubscriptionCancelled,
+	formatSubscriptionStarted,
+	type SlackBillingEventType,
+} from "./slack-billing";
