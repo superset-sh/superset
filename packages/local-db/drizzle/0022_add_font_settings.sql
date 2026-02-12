842d3ba6920360c098c9830d7a943b1881bfa6a2
Kiet Ho <hoakiet98@gmail.com>
2026-02-11 23:30:40 -0800

    chore: remove font settings migration for regeneration

--- packages/local-db/drizzle/0022_add_font_settings.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `settings` ADD `terminal_font_family` text;--> statement-breakpoint
-ALTER TABLE `settings` ADD `terminal_font_size` integer;--> statement-breakpoint
-ALTER TABLE `settings` ADD `editor_font_family` text;--> statement-breakpoint
-ALTER TABLE `settings` ADD `editor_font_size` integer;
