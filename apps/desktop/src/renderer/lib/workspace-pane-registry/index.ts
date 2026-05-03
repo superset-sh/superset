7e782551338fdcd438f25c106751ac58ef0ab1c6
Kiet Ho <hoakiet98@gmail.com>
2026-05-03 15:29:59 -0700

    Revert "feat(desktop): workspace pane store registry (v2 PR 3) (#3940)"
    This reverts commit f7db05ce8c33e5f9937326c3f1469914654a2aa8.

--- apps/desktop/src/renderer/lib/workspace-pane-registry/index.ts
+++
@@ -1,7 +0,0 @@
-export { addLaunchPanes, type LaunchPaneInput } from "./addLaunchPanes";
-export {
-	dropWorkspacePaneStore,
-	getOrCreateWorkspacePaneStore,
-	initWorkspacePaneRegistry,
-	type WorkspacePaneRegistryDeps,
-} from "./workspace-pane-registry";
